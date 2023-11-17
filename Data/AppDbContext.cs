aaa2eaab886593640ebeab9cd979004518307a7d
Marcos Gonçalves <marcos.goncalves@avanade.com>
2023-11-17 17:50:33 -0300

    Feat: Managing Relationship

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -6,9 +6,11 @@ namespace NarrativusAPI.Data
     public class AppDbContext : DbContext
     {
         public DbSet<Location> Locations { get; set; }
+        public DbSet<Character> Characters { get; set; }
+        public DbSet<Relationship> Relationships { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         => optionsBuilder.UseSqlite("DataSource=app.db;Cache=Shared");
-        
+
     }
 }
